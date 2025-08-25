bcae4cbdbd88ab5bd596d595cf652049f6c610bd
Gabriel Henrique. <gabrielhenriques4@gmail.com>
2025-08-25 16:24:58 -0300

    Update Ex1par_impar.py

--- Python/Ex1par_impar.py
+++ Python/Ex1par_impar.py
@@ -1,1 +1,24 @@
+def main():
+    """""   
+    função para verificar se um numero é impar ou par  
 
+    função recebe um parametro (n) 
+    numero de entrada colocado pelo usuario.
+
+    retorna:
+    a condição n % 2 == 0 retorna as condições em que se o numero de entrada
+    é positivo ou falso (par ou impar) ele mostrará o resultado
+
+    """""
+    def impar_par(n):
+        if n % 2 == 0:
+            return("par")
+        else:
+            return("Impar") 
+    entrada = int(input("Digite um numero inteiro: "))
+    resultado = impar_par(entrada)
+    print(f"O numero {entrada} é {resultado}")
+if __name__ == "__main__":
+    main()   
+   
+             
