0de852cbb935f50db8dcc11d732ff856900bebd5
Gabriel Henrique. <gabrielhenriques4@gmail.com>
2025-08-25 16:31:00 -0300

    Update Ex3.py

--- Python/Ex3.py
+++ Python/Ex3.py
@@ -1,1 +1,10 @@
+# Maior ou menor 
 
+def main(): 
+    idade = int(input("Idade: "))
+    if idade >= 18:
+        print("Maior de idade")
+    else:
+        print("Menor de idade")
+if __name__ == "__main__":
+    main()  
