94a4f32c6519976bcdb2a3c4b026913253aa6d4b
Gabriel Henrique. <gabrielhenriques4@gmail.com>
2025-08-25 21:11:51 -0300

    Create ex9defparimpar.py

---
+++ Python/ex9defparimpar.py
@@ -0,0 +1,21 @@
+def main()
+
+    def parImpar(n):
+        if n % 2 == 0:
+            return ("Par")
+        else:
+            return ("Impar")
+        
+    
+    entrada   = int(input("Digite um numero: "))
+    resultado = parImpar(entrada)
+    print(f"O numero {entrada} é {resultado}")
+    
+    pessoa = {
+        "nome": input(...),
+        
+    }
+    
+    
+if __name__ == "__main__":
+    main()
