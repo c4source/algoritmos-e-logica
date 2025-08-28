a27a99a14e2efaab3e6d05b5591f8ab1782b309d
Gabriel Henrique. <204272661+c4source@users.noreply.github.com>
2025-08-28 17:47:59 -0300

    Create MundoPythonEx2.py

---
+++ Python/MundoPythonEx2.py
@@ -0,0 +1,9 @@
+import math
+
+n = float(input("Numero: "))
+dobro = n * 2 
+triplo = n * 3 
+raizquadrada =  math.sqrt(n)
+print(f"O dobro de {n}:", dobro)
+print(f"O triplo de {n}:", triplo)
+print(f"A raiz quadrada de {n} é {raizquadrada:.2f}")
