3b465a578fd88c35bca2146393846dd0ae8ca29a
Gabriel Henrique. <204272661+c4source@users.noreply.github.com>
2025-09-01 17:19:33 -0300

    Update MundoPythonDesconto.py

--- Python/MundoPythonDesconto.py
+++ Python/MundoPythonDesconto.py
@@ -1,1 +1,12 @@
+def main():
+    print("\n == Aplique 5% de desconto ==")
+    produto  = float(input("Coloque o preço: ")) #   x * 0.05 
+    desconto = produto * 0.05
+    produtoCmDesconto = produto - desconto
+    print(f"Valor do produto: {produto:.2f}")
+    print(f"Valor com 5% de desconto: {produtoCmDesconto:.2f}")
 
+
+
+if __name__ == "__main__":
+    main()    
