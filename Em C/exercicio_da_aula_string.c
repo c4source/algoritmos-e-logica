1dbab434b4db5bb37884b2fa947cfe65b8fb0ed9
Gabriel Henrique. <204272661+c4source@users.noreply.github.com>
2025-09-10 16:57:00 -0300

    Update exercicio_da_aula_string.c

--- Em C/exercicio_da_aula_string.c
+++ Em C/exercicio_da_aula_string.c
@@ -5,35 +5,43 @@ int main(){
     char nome[51];
     char sobrenome[51];
     char email[51];
-    int datanascimento, celular;
+    char datanascimento[10], celular[16];
     
     printf("Digite seu nome: ");
     fgets(nome, sizeof(nome), stdin);
     nome[strcspn(nome, "\n")] = '\0';
-    printf("Nome: %s\n", nome); 
+     
     
-    
-    printf("Digite seu sobre nome: ");
+    //funçao Fgets para ler string --> + seguro 
+    printf("Digite seu sobrenome: ");
     fgets(sobrenome, sizeof(sobrenome), stdin);
     sobrenome[strcspn(sobrenome, "\n")] = '\0';
-    printf("Sobrenome: %s\n", sobrenome);
+   
     
     printf("Digite seu email: ");
     fgets(email, sizeof(email), stdin);
     email[strcspn(email, "\n")] = '\0';
-    printf("Email: %s\n", email);
+    
     
     printf("Digite seu numero de telefone: ");
-    scanf("%d", &celular);
-    printf("Celular: %d\n", celular);
-    getchar();
+    fgets(celular, sizeof(celular), stdin);
+    celular[strcspn(email, "\n")] = '\0'; 
+    
+    
     
     printf("Digite Sua data de nascimento: ");
-    scanf("%d", &datanascimento);
-    printf("Data de nascimento %d\n", datanascimento);
-    getchar();
-    return 0; 
+    fgets(datanascimento, sizeof(datanascimento), stdin); 
+    datanascimento[strcspn(datanascimento, "\n")] = '\0';
+
     
+    printf("=== Lista de dados ===\n");
+    strcat(nome, " ");
+    strcat(nome, sobrenome);
+    printf("Nome: %s\n", nome);
+    printf("Sobrenome: %s\n", sobrenome);
+    printf("Email: %s\n", email);
+    printf("Data de nascimento %s\n", datanascimento);
     
-    
+    return 0;
+        
 }    
