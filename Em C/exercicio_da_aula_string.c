d9ce136876a802ba3ac071b5de63c3f6b481b1ee
Gabriel Henrique. <204272661+c4source@users.noreply.github.com>
2025-09-09 21:07:14 -0300

    Update exercicio_da_aula_string.c

--- Em C/exercicio_da_aula_string.c
+++ Em C/exercicio_da_aula_string.c
@@ -1,1 +1,39 @@
-
+#include <stdio.h>
+#include <string.h> 
+int main(){
+    
+    char nome[51];
+    char sobrenome[51];
+    char email[51];
+    int datanascimento, celular;
+    
+    printf("Digite seu nome: ");
+    fgets(nome, sizeof(nome), stdin);
+    nome[strcspn(nome, "\n")] = '\0';
+    printf("Nome: %s\n", nome); 
+    
+    
+    printf("Digite seu sobre nome: ");
+    fgets(sobrenome, sizeof(sobrenome), stdin);
+    sobrenome[strcspn(sobrenome, "\n")] = '\0';
+    printf("Sobrenome: %s\n", sobrenome);
+    
+    printf("Digite seu email: ");
+    fgets(email, sizeof(email), stdin);
+    email[strcspn(email, "\n")] = '\0';
+    printf("Email: %s\n", email);
+    
+    printf("Digite seu numero de telefone: ");
+    scanf("%d", &celular);
+    printf("Celular: %d\n", celular);
+    getchar();
+    
+    printf("Digite Sua data de nascimento: ");
+    scanf("%d", &datanascimento);
+    printf("Data de nascimento %d\n", datanascimento);
+    getchar();
+    return 0; 
+    
+    
+    
+}    
