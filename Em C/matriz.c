3f11436dccb3ce2b32a18053fbfb8751c75aa012
Gabriel Henrique. <204272661+c4source@users.noreply.github.com>
2025-09-30 19:49:18 -0300

    Update and rename matriz,c to matriz.c

---
+++ Em C/matriz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define L 3 
+#define C 3 
+
+int main(){
+    int mat[L][C];
+        for (int i = 0; i < L; i++){
+            for (int j = 0; j < C; i++){
+                scanf("%d", &mat[i][j]);
+            }
+        }
+    return 0; 
+}  
