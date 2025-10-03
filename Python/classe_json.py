d907bebff23f218ce99a6e80abc111d28a43706f
Gabriel Henrique. <204272661+c4source@users.noreply.github.com>
2025-10-02 21:47:44 -0300

    Update classe_json.py

--- Python/classe_json.py
+++ Python/classe_json.py
@@ -25,6 +25,35 @@ print(lista)
 with open("dados.json", "w") as arquivo:
     json.dump(lista,arquivo,indent=4)
     
+#Atributos privados
+
+import json 
+
+class Contatos():
+    def __init__(self, nome, email, cpf, telefone):
+        self.__nome = nome
+        self.__email = email 
+        self.__cpf = cpf 
+        self.__telefone = telefone
+        
+    def to_dict(self):
+        
+        return {
+            'nome':self.__nome,
+            'email':self.__email,
+            'cpf':self.__cpf,
+            'telefone':self.__telefone, 
+        }      
+
+contato_1 = Contatos("Gabriel", "gabrielhenriques69@gmai.com", "cpf", "telefone") 
+
+lista = []
+lista.append(contato_1.to_dict())
+print(lista)
+
+with open("dados.json", "w") as arquivo:
+    json.dump(lista,arquivo,indent=4)
+    
 
 
 
