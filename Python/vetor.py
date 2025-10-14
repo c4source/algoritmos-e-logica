eb2f57dc9c0759db17af65b483260013260597b3
c4source <gabrielhenriques4@gmail.com>
2025-10-13 21:39:23 -0300

    Buscas conceitos e atividades

--- Python/vetor.py
+++ Python/vetor.py
@@ -10,6 +10,24 @@ for i in range(len(numeros)-1):
         if numeros[j]>numeros[j+1]:
             numeros[j], numeros[j+1]=numeros[j+1],numeros[j]
 
-print("Vetor ordenado: ", numeros)            
+print("Vetor ordenado: ", numeros) 
+
+
+#Fazer um algortimo de busca linear 
+#Gerar um numero randomico no vetor
+#Criar uma funcao p fzr uma busca linear 
+#Se encontrou é zero, senao encontrou é 1
+
+
+#Buscas: Linear (Sequencial), 
+# Busca binária)
+
+# dados = [5, 2, 1, 4, 3]
+
+#Busca binária (Grande volume de dados)
+#1 - ordenação
+#2 - len(dados)/2 = 2,5  ---> func de fzr busca. Dividir pela metade o numero que vc deseja encontrar 
+
+#27 do dez entregar as atividades 
 
 
